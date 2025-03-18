487c3cd793cf749c2f1d54485405a7c2e675ed6b
Railly <railly_2009@hotmail.com>
2025-03-17 21:56:19 -0500

    feat: revamp UI + first guide draft

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,32 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import mdx from '@astrojs/mdx';
+import vesperDark from "./public/theme/vesper-dark.json";
+import vesperLight from "./public/theme/vesper-light.json";
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+  },
+  integrations: [mdx()],
+  markdown: {
+    shikiConfig: {
+      themes: {
+        // @ts-ignore
+        light: vesperLight,
+        // @ts-ignore
+        dark: vesperDark,
+      },
+      langs: [],
+      transformers: [
+        {
+          line(node, line) {
+            node.properties["data-line"] = line;
+            this.addClassToHast(node, "line");
+          },
+        },
+      ],
+    },
   },
 });
